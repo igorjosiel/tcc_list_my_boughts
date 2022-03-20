14cd515ed6ee7a495c21ec1c9b88e53bca843536
Igor Josiel <igorjosielvitalino@gmail.com>
2022-03-20 01:42:38 -0300

    feat: adding the app navigation.

--- App.tsx
+++ App.tsx
@@ -1,44 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { useSetFonts } from './hooks/useSetFonts';
-import { welcomeMessage } from './utils/messages';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import FirstScreen from './screens/FirstScreen/FirstScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const italic = useSetFonts('Poppins_400Regular');
-
   return (
-    <View style={styles.container}>
-      <View style={{ margin: '10px', width: '80%', backgroundColor: '#87CEEB', borderRadius: 5, marginBottom: '200px' }}>
-        <Text style={{ fontFamily: italic.toString(), padding: '10px', fontSize: 16 }}>{welcomeMessage}</Text>
-      </View>
-      <View style={{ width: '80%' }}>
-        <TouchableOpacity onPress={() => alert('Hello, world!')} style={styles.button}>
-          <Text style={styles.buttonText}>Entrar</Text>
-        </TouchableOpacity>
-      </View>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='FirstScreen'>
+        <Stack.Screen name="FirstScreen" component={FirstScreen}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // backgroundColor: '#ADD8E6',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button: {
-    backgroundColor: "blue",
-    width: '30vw',
-    height: '13vw',
-    padding: 20,
-    borderRadius: 5,
-    margin: 'auto'
-  },
-  buttonText: {
-    fontSize: 16,
-    textTransform: 'uppercase',
-    color: '#fff',
-  },
-});
