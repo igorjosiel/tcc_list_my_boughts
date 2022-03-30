31733d77db64bc184408d977483b70b86cb8cd35
Igor Josiel <igorjosielvitalino@gmail.com>
2022-03-29 22:53:10 -0300

    feat: adding states in the screen to store the items of the list.

--- screens/ListItens/ListItens.tsx
+++ screens/ListItens/ListItens.tsx
@@ -12,7 +12,29 @@ const ListItens = () => {
     const Poppins_400Regular = useSetFonts('Poppins_600SemiBold');
     const Poppins_600SemiBold = useSetFonts('Poppins_600SemiBold');
 
-    const [isChecked, setChecked] = useState(false);
+    interface Item {
+        id: number,
+        name: string,
+        checked: boolean,
+    }
+
+    const [listItems, setListItems] = useState<Item[]>([]);
+    const [count, setCount] = useState<number>(0);
+
+    const addItemToList = (item: string) => {
+        const newItem = {
+            id: count,
+            name: item,
+            checked: false,
+        };
+
+        setListItems(oldState => [...oldState, newItem]);
+        setCount(oldState => oldState + 1);
+    }
+
+    const changeItemOfList = (id: number) => {
+        console.log('Chegou: ', id);
+    }
 
     return (
         <ImageBackground source={imagePaper} style={styles.image}>
@@ -20,7 +42,16 @@ const ListItens = () => {
                 <Text fontFamily={Poppins_600SemiBold} fontSize={28}>Lista de Compras</Text>
             </View>
             <View style={{ height: '80%', padding: '10%', paddingTop: 0 }}>
-                <Conatiner style={styles.section}>
+                {listItems?.map((item => {
+                    return (
+                        <Conatiner style={styles.section}>
+                            <Checkbox value={item.checked} onValueChange={() => changeItemOfList(item.id)} />
+                            <Text fontFamily={Poppins_400Regular} fontSize={22}>{item.name}</Text>
+                            <Icon size={26} color="#000" name="pencil" />
+                        </Conatiner>
+                    );
+                }))}
+                {/* <Conatiner style={styles.section}>
                     <Checkbox value={isChecked} onValueChange={setChecked} />
                     <Text fontFamily={Poppins_400Regular} fontSize={22}>Arroz</Text>
                     <Icon size={26} color="#000" name="pencil" />
@@ -29,7 +60,7 @@ const ListItens = () => {
                     <Checkbox value={isChecked} onValueChange={setChecked} />
                     <Text fontFamily={Poppins_400Regular} fontSize={22}>Feijão</Text>
                     <Icon size={26} color="#000" name="pencil" />
-                </Conatiner>
+                </Conatiner> */}
                 {/* <Conatiner style={styles.section}>
                     <Checkbox value={isChecked} onValueChange={setChecked} />
                     <Text style={{ fontFamily: Poppins_400Regular, fontSize: 22 }}>Leite</Text>
@@ -37,7 +68,7 @@ const ListItens = () => {
                 </Conatiner> */}
             </View>
             <View style={{ height: '10%', alignItems: 'flex-end', marginRight: '20px' }}>
-                <Pressable style={{ backgroundColor: '#1E90FF', height: 70, width: 70, borderRadius: '50%', alignItems: 'center', justifyContent: 'center' }}>
+                <Pressable style={{ backgroundColor: '#1E90FF', height: 55, width: 55, borderRadius: '50%', alignItems: 'center', justifyContent: 'center' }}>
                     <Icon size={40} color="#000" name="plus" />
                 </Pressable>
             </View>
