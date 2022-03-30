fbc8c53a78b0bf92dcf022e92772a721be9838ff
Igor Josiel <igorjosielvitalino@gmail.com>
2022-03-29 23:47:40 -0300

    fix: adjusting the size of the fonts.

--- screens/ListItens/ListItens.tsx
+++ screens/ListItens/ListItens.tsx
@@ -23,7 +23,7 @@ const ListItens = () => {
     const [listItems, setListItems] = useState<Item[]>([]);
     const [count, setCount] = useState<number>(0);
 
-    const addItemToList = (item: string) => {
+    const addItemToList = () => {
         const newItem = {
             id: count,
             name: item,
@@ -32,6 +32,7 @@ const ListItens = () => {
 
         setListItems(oldState => [...oldState, newItem]);
         setCount(oldState => oldState + 1);
+        setItem('');
     }
 
     const changeItemOfList = (id: number) => {
@@ -46,14 +47,14 @@ const ListItens = () => {
         // <ImageBackground source={imagePaper} style={styles.image}>
         <>
             <View style={{ height: '10%', marginLeft: 'auto', marginRight: 'auto', justifyContent: 'center' }}>
-                <Text fontFamily={Poppins_600SemiBold} fontSize={28}>Lista de Compras</Text>
+                <Text fontFamily={Poppins_600SemiBold} fontSize={26}>Lista de Compras</Text>
             </View>
             <View style={{ height: '75%', padding: '10%', paddingTop: 0 }}>
                 {listItems?.map((item => {
                     return (
                         <Conatiner style={styles.section}>
                             <Checkbox value={item.checked} onValueChange={() => changeItemOfList(item.id)} />
-                            <Text fontFamily={Poppins_400Regular} fontSize={22}>{item.name}</Text>
+                            <Text fontFamily={Poppins_400Regular} fontSize={20}>{item.name}</Text>
                             <Icon size={26} color="#000" name="pencil" />
                         </Conatiner>
                     );
@@ -74,7 +75,7 @@ const ListItens = () => {
                     <Icon size={26} color="#000" name="pencil" />
                 </Conatiner> */}
             </View>
-            <View style={{ height: '15%', alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between', marginBottom: '10px', marginRight: '10px', marginLeft: '10px' }}>
+            <View style={{ height: '15%', alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between', marginBottom: '10px', marginRight: '20px', marginLeft: '10px' }}>
                 <TextInput
                     style={styles.input}
                     onChangeText={(value) => editItemName(value)}
@@ -82,7 +83,7 @@ const ListItens = () => {
                     placeholder="Digite aqui o protudo"
                     keyboardType="numeric"
                 />
-                <Pressable style={{ backgroundColor: '#1E90FF', height: 55, width: 55, borderRadius: '50%', alignItems: 'center', justifyContent: 'center' }}>
+                <Pressable onPress={() => addItemToList()} style={{ backgroundColor: '#1E90FF', height: 55, width: 55, borderRadius: '50%', alignItems: 'center', justifyContent: 'center' }} >
                     <Icon size={40} color="#000" name="plus" />
                 </Pressable>
             </View>
