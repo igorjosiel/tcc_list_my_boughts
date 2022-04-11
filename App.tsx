c6790c46bcbed3482372d3cb4956995be1c98dbc
Igor Josiel <igorjosielvitalino@gmail.com>
2022-04-11 00:10:28 -0300

    fix: calling the screens.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,28 @@
 import { ThemeProvider } from "styled-components";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import theme from './global/styles/theme';
 import ListItens from "./screens/ListItens/ListItens";
+import ProductsPage from "./screens/ProductsPage/ProductsPage";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <ListItens />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name='ListItens'
+            component={ListItens}
+          />
+          <Stack.Screen
+            name='ProductsPage'
+            component={ProductsPage}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </ThemeProvider>
-  )
+  );
 }
