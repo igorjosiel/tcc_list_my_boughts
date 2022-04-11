12891c6adbfd2c38623b2a0bc84d2106913c704d
Igor Josiel <igorjosielvitalino@gmail.com>
2022-04-11 00:08:48 -0300

    fix: creating another screen to put all the data about each product that is added in the app.

--- screens/ListItens/ListItens.tsx
+++ screens/ListItens/ListItens.tsx
@@ -1,45 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import { Conatiner } from "./ListItens.styles";
-import { StyleSheet, Pressable, View, TextInput, ScrollView } from "react-native";
+import { StyleSheet, Pressable, View, ScrollView, ImageBackground } from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { ImageBackground } from 'react-native';
-
 
 import { useSetFonts } from "../../hooks/useSetFonts";
 import Text from '../../components/Text/Text';
 import theme from '../../global/styles/theme';
 import compras from '../../assets/compras.jpg';
 
-const ListItens = () => {
+const ListItens = ({ navigation }) => {
     const Poppins_400Regular = useSetFonts('Poppins_600SemiBold');
     const Poppins_600SemiBold = useSetFonts('Poppins_600SemiBold');
 
     interface Item {
         id: number,
-        amount: number,
-        name: string,
-        made: boolean,
+        amount: string,
+        product: string,
         category: string,
+        price: number,
     }
 
-    const [item, setItem] = useState<string>('');
     const [listItems, setListItems] = useState<Item[]>([]);
-    const [count, setCount] = useState<number>(0);
+    const [idGenerator, setIdGenerator] = useState<number>(0);
+    const [totalValue, setTotalValue] = useState<number>(0);
 
-    const addItemToList = () => {
+    const addItemToList = (item: Item) => {
         if (!item) return;
 
         const newItem = {
-            id: count,
-            amount: 1,
-            name: item,
-            made: false,
-            category: 'teste',
+            ...item,
+            id: idGenerator,
         };
 
+        setTotalValue(oldState => oldState + (item?.price * parseInt(item?.amount)));
         setListItems(oldState => [...oldState, newItem]);
-        setCount(oldState => oldState + 1);
-        setItem('');
+        setIdGenerator(oldState => oldState + 1);
     }
 
     const removeItemToList = (id: number) => {
@@ -68,19 +63,6 @@ const ListItens = () => {
         setListItems(newItems);
     };
 
-    const changeItemStatus = (id: number) => {
-        const changedItem = listItems?.map(item => item?.id === id ? {
-            ...item,
-            made: !item.made,
-        } : item);
-
-        setListItems(changedItem);
-    }
-
-    const editItemName = (itemName: string) => {
-        setItem(itemName);
-    }
-
     return (
         <>
             <View style={{
@@ -106,14 +88,14 @@ const ListItens = () => {
                     <ScrollView>
                         {listItems?.map(((item, index) => {
                             return (
-                                <Conatiner style={item?.made ? styles.made : styles.section} key={index}>
-                                    <Pressable style={{ width: '65%' }} onPress={() => changeItemStatus(item?.id)}>
-                                        <Text style={item?.made && styles.tachado} fontFamily={Poppins_400Regular} fontSize={20}>{item?.name}</Text>
+                                <Conatiner style={styles.section} key={index}>
+                                    <Pressable style={{ width: '65%' }}>
+                                        <Text fontFamily={Poppins_400Regular} fontSize={20}>{item?.product}</Text>
                                     </Pressable>
                                     {/* <Checkbox value={item?.checked} onValueChange={() => changeItemOfList(item?.id)} /> */}
                                     <View style={{ width: '45%', flex: 1, flexDirection: 'row', justifyContent: 'space-between', backgroundColor: 'white', borderRadius: 5 }}>
                                         {/* <Icon size={28} style={{ color: theme.colors.primary, marginBottom: 'auto', marginTop: 'auto' }} name="minus" /> */}
-                                        <Pressable onPress={item?.amount === 1 ? () => removeItemToList(item?.id) : () => decreaseItemAmount(item?.id)} style={{
+                                        <Pressable onPress={item?.amount === "1" ? () => removeItemToList(item?.id) : () => decreaseItemAmount(item?.id)} style={{
                                             width: 25,
                                             borderRadius: 50,
                                             alignItems: 'center',
@@ -139,20 +121,34 @@ const ListItens = () => {
                 </ImageBackground>
             </View>
             <View style={{ height: '10%', backgroundColor: 'white', paddingTop: '3%', alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10, marginRight: 20, marginLeft: 10 }}>
-                <TextInput
+                {/* <TextInput
                     style={styles.input}
                     onChangeText={(value) => editItemName(value)}
                     value={item}
                     placeholder="Digite aqui o protudo"
                     keyboardType="default"
-                />
-                <Pressable onPress={() => addItemToList()} style={{
+                /> */}
+                <View style={{
+                    backgroundColor: theme?.colors?.primary,
+                    width: '70%',
+                    height: '80%',
+                    borderRadius: 10,
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'flex-end',
+                    paddingRight: '3%',
+                    paddingLeft: '3%',
+                }}>
+                    <Text fontFamily={Poppins_600SemiBold} fontSize={25}>R$ {totalValue}</Text>
+                </View>
+                <Pressable onPress={() => navigation.navigate('ProductsPage', { addItemToList: (newItem: object) => addItemToList(newItem) })} style={{
                     backgroundColor: theme.colors.primary,
                     height: 55,
                     width: 55,
                     borderRadius: 50,
                     alignItems: 'center',
-                    justifyContent: 'center'
+                    justifyContent: 'center',
                 }}>
                     <Icon size={30} color="#000" name="plus" />
                 </Pressable>
@@ -205,17 +201,6 @@ const styles = StyleSheet.create({
         margin: 8,
         height: 20,
         width: 20,
-    },
-    input: {
-        height: 60,
-        width: '60%',
-        margin: '2%',
-        borderWidth: 3,
-        borderColor: theme.colors.primary,
-        padding: 10,
-        borderRadius: 10,
-        fontSize: 20,
-        shadowOffset: { width: 0, height: 1 }
     },
 })
 
