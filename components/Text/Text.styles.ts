732c438586426d9c831270cef3e18f289766afb7
Igor Josiel <igorjosielvitalino@gmail.com>
2022-05-05 12:31:51 -0300

    feat: adding a new CSS property to the Text component.

--- components/Text/Text.styles.ts
+++ components/Text/Text.styles.ts
@@ -3,8 +3,9 @@ import styled from "styled-components/native";
 import { TextProps } from "./Text.types";
 
 const StyledText = styled.Text<TextProps>`
-    font-family: ${props => props?.fontFamily};
-    font-size: ${props => props?.fontSize};
+  font-family: ${(props) => props?.fontFamily};
+  font-size: ${(props) => props?.fontSize};
+  color: ${(props) => (props?.color ? props?.color : "#FFF")};
 `;
 
 export default StyledText;
