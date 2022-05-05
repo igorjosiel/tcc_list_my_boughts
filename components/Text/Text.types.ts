5cdd5a393284e93bea2f977394cc2ec81991a72f
Igor Josiel <igorjosielvitalino@gmail.com>
2022-05-05 12:29:25 -0300

    feat: adding a new property on Text component.

--- components/Text/Text.types.ts
+++ components/Text/Text.types.ts
@@ -2,7 +2,8 @@ import { ReactNode } from "react";
 import { TextProps as Props } from "react-native";
 
 export interface TextProps extends Props {
-    fontFamily: string,
-    fontSize: number,
-    children: ReactNode | string | number,
+  fontFamily: string;
+  fontSize: number;
+  color?: string;
+  children: ReactNode | string | number;
 }
