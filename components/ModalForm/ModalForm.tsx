d3337d05f39d435930f7895faf1221c0a3421bef
Igor Josiel <igorjosielvitalino@gmail.com>
2022-05-09 17:37:26 -0300

    feat: consuming the new component for Input.

--- components/ModalForm/ModalForm.tsx
+++ components/ModalForm/ModalForm.tsx
@@ -1,38 +1,63 @@
+import { useState } from "react";
 import { Modal } from "react-native";
 
 import { CenteredView, ModalView, ModalTitle } from "./ModalForm.styles";
-import { ModalFormProps } from "./ModalForm.types";
 import Text from "../Text/Text";
+import TextInput from "../Input/TextInput/TextInput";
+
+import { ModalFormProps } from "./ModalForm.types";
+import { Product } from "../../utils/interfaces";
 
 import { useSetFonts } from "../../hooks/useSetFonts";
 
 const ModalForm: React.FC<ModalFormProps> = (props: ModalFormProps) => {
-    const Poppins_600SemiBold = useSetFonts("Poppins_600SemiBold");
+  const [newProduct, setNewProduct] = useState<Product>({
+    id: 0,
+    amount: 1,
+    productName: "",
+    category: "",
+    price: 0,
+    priority: false,
+  });
 
-    const { isModalOpen, closeModal } = props;
+  const Poppins_600SemiBold = useSetFonts("Poppins_600SemiBold");
 
-    return (
-        <Modal
-            animationType="slide"
-            transparent={true}
-            visible={isModalOpen}
-            onRequestClose={closeModal}
-        >
-            <CenteredView>
-                <ModalView>
-                    <ModalTitle>
-                        <Text
-                            fontFamily={Poppins_600SemiBold}
-                            fontSize={22}
-                            color={"#000"}
-                        >
-                            Informações do Produto
-                        </Text>
-                    </ModalTitle>
-                </ModalView>
-            </CenteredView>
-        </Modal>
-    );
+  const { isModalOpen, closeModal } = props;
+
+  const setPropertyNewProduct = (
+    value: string | number | boolean | null,
+    property: string
+  ) => {
+    setNewProduct({ ...newProduct, [property]: value });
+  };
+
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={isModalOpen}
+      onRequestClose={closeModal}
+    >
+      <CenteredView>
+        <ModalView>
+          <ModalTitle>
+            <Text fontFamily={Poppins_600SemiBold} fontSize={22} color={"#000"}>
+              Informações do Produto
+            </Text>
+          </ModalTitle>
+          <TextInput
+            fontFamily={Poppins_600SemiBold}
+            onChangeText={(value) =>
+              setPropertyNewProduct(value, "productName")
+            }
+            value={newProduct?.productName}
+            placeholder="Nome do produto"
+            keyboardType="default"
+          />
+        </ModalView>
+      </CenteredView>
+    </Modal>
+  );
 };
 
 export default ModalForm;
