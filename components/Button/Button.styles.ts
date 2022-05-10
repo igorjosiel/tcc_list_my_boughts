7cf980f15c496af197b182dd5f3f678f8270ae60
Igor Josiel <igorjosielvitalino@gmail.com>
2022-05-10 18:01:22 -0300

    fix: adjusting the button component with more properties.

--- components/Button/Button.styles.ts
+++ components/Button/Button.styles.ts
@@ -5,6 +5,7 @@ import ButtonProps from "./Button.types";
 const StyledButton = styled.TouchableOpacity<ButtonProps>`
   background-color: ${(props) => props?.backgroundColor};
   height: ${(props) => props?.height};
+  min-height: ${(props) => props?.minHeight};
   width: ${(props) => props?.width};
   border-bottom-left-radius: ${(props) => props?.borderBottomLeftRadius};
   border-bottom-right-radius: ${(props) => props?.borderBottomRightRadius};
@@ -14,6 +15,13 @@ const StyledButton = styled.TouchableOpacity<ButtonProps>`
   flex-direction: ${(props) => props?.flexDirection};
   justify-content: ${(props) => props?.justifyContent};
   align-items: ${(props) => props?.alignItems};
+  border-width: ${(props) => props?.borderWidth};
+  border-color: ${(props) => props?.borderColor};
+  border-radius: ${(props) => props?.borderRadius};
+  border-bottom-color: ${(props) => props?.borderBottomColor};
+  border-bottom-width: ${(props) => props?.borderBottomWidth};
+  padding: ${(props) => (props?.padding ? props?.padding : "0px")};
+  margin-bottom: ${(props) => props?.marginBottom};
 `;
 
 export default StyledButton;
--- components/Button/Button.types.ts
+++ components/Button/Button.types.ts
@@ -1,14 +1,22 @@
 import { ReactNode } from "react";
 import { TouchableOpacityProps } from "react-native";
 export default interface ButtonProps extends TouchableOpacityProps {
-  backgroundColor: string;
-  height: string;
-  width: string;
+  backgroundColor?: string;
+  height?: string;
+  minHeight?: string;
+  width?: string;
   borderBottomLeftRadius?: string;
   borderBottomRightRadius?: string;
   borderTopLeftRadius?: string;
   borderTopRightRadius?: string;
-  display: string;
+  borderWidth?: number;
+  borderColor?: string;
+  borderRadius?: string;
+  borderBottomColor?: string;
+  borderBottomWidth?: string;
+  padding?: string;
+  marginBottom?: string;
+  display?: string;
   flexDirection: string;
   justifyContent: string;
   alignItems: string;
