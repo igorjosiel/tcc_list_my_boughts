351f7b4d206b7bde0fba3b84c09e21c09de650fa
Igor Josiel <igorjosielvitalino@gmail.com>
2022-05-10 11:57:53 -0300

    fix: refactoring the button component.

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,8 +2,8 @@ import ButtonProps from "./Button.types";
 
 import StyledButton from "./Button.styles";
 
-const Button = (props: ButtonProps) => {
-  return <StyledButton {...props}></StyledButton>;
+const Button: React.FC<ButtonProps> = (props: ButtonProps) => {
+  return <StyledButton {...props} />;
 };
 
 export default Button;
