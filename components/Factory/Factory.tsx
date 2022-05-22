df7e4d0aab637d081fb7d0f7ce178b3227aba8a3
Igor Josiel <igorjosielvitalino@gmail.com>
2022-05-21 23:16:08 -0300

    feat: adding more categories to the app.

--- components/Factory/Factory.tsx
+++ components/Factory/Factory.tsx
@@ -2,6 +2,8 @@ import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import AntDesign from "react-native-vector-icons/AntDesign";
+import Fontisto from "react-native-vector-icons/Fontisto";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
 import FactoryProps from "./FactoryProps";
 
@@ -11,8 +13,12 @@ const Factory = ({ library, icon }: FactoryProps) => {
             return <MaterialIcons name={icon} size={30} color={"#FFF"} />;
         case 'FontAwesome':
             return <FontAwesome name={icon} size={30} color={"#FFF"} />;
+        case 'FontAwesome5':
+            return <FontAwesome5 name={icon} size={30} color={"#FFF"} />;
         case 'AntDesign':
             return <AntDesign name={icon} size={30} color={"#FFF"} />;
+        case 'Fontisto':
+            return <Fontisto name={icon} size={30} color={"#FFF"} />
         case 'MaterialCommunityIcons':
             return <MaterialCommunityIcons name={icon} size={30} color={"#FFF"} />;
         default: return <FontAwesome name={icon} size={30} color={"#FFF"} />;
